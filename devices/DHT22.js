fe74af9a09041fa581a7f4da064551bd70df595f
Manfred Kröhnert <7868+mkroehnert@users.noreply.github.com>
2022-03-23 22:15:52 +0100

    Update DHT22.js
    Update DHT22.js module as per discussion in http://forum.espruino.com/comments/14870784/ 
    
    see also https://github.com/espruino/Espruino/issues/1687

--- devices/DHT22.js
+++ devices/DHT22.js
@@ -4,6 +4,7 @@ This module interfaces with a DHT22 temperature and relative humidity sensor.
 Usage (any GPIO pin can be used):
 
 var dht = require("DHT22").connect(C11);
+// default number of retries is 1
 dht.read(function (a) {console.log("Temp is "+a.temp.toString()+" and RH is "+a.rh.toString());});
 
 the return value if no data received: {"temp": -1, "rh": -1, err:true, "checksumError": false}
@@ -15,20 +16,19 @@ function DHT22(pin) {
 }
 
 DHT22.prototype.read = function (cb, n) {
-  if (!n) n=10;
+  if (!n) n=1;
   var d = "";
   var ht = this;
+  // force pin state to output
+  pinMode(ht.pin,"output");
   digitalWrite(ht.pin, 0);
-  pinMode(ht.pin,"output"); // force pin state to output
-  // start watching for state change
+  // read d based on signal change interval
   this.watch = setWatch(function(t) {
     d+=0|(t.time-t.lastTime>0.00005);
   }, ht.pin, {edge:'falling',repeat:true} );
-  // raise pulse after 1ms
-  setTimeout(function() {pinMode(ht.pin,'input_pullup');pinMode(ht.pin);},1);
   // stop looking after 50ms
   setTimeout(function() {
-    if(ht.watch){ ht.watch = clearWatch(ht.watch); }
+    clearWatch(ht.watch);
     var cks =
         parseInt(d.substr(2,8),2)+
         parseInt(d.substr(10,8),2)+
@@ -45,6 +45,9 @@ DHT22.prototype.read = function (cb, n) {
       else cb({err:true, checksumError:cks>0, raw:d, temp:-1, rh:-1});
     }
   }, 50);
+  // raise pulse after everything is setup
+  pinMode(ht.pin,'input_pullup');
+  pinMode(ht.pin);
 };
 
 exports.connect = function(pin) {
